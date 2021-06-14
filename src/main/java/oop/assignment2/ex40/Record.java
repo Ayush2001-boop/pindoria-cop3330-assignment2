3c7873685df10b2d962ab44954c137996514f296
Ayush <ayushpindoria2001@knigghts.ucf.edu>
2021-06-13 22:36:45 -0400

    Assignment2 commit

---
+++ src/main/java/oop/assignment2/ex40/Record.java
@@ -0,0 +1,19 @@
+package oop.assignment2.ex40;
+import java.util.Date;
+
+public class Record
+{
+    String firstName;
+    String lastName;
+    String position;
+    Date seperationDate;
+
+    public Record(String firstName, String lastName,
+                  String position, Date seperationDate)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+        this.seperationDate = seperationDate;
+    }
+}
